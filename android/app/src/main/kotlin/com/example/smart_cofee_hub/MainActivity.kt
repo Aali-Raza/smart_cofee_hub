28c900439cd14d5e9d9d8177d92b49dda455e3c2
Fast Computers <aliraza121465@gmail.com>
2023-06-10 05:16:50 -0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/smart_cofee_hub/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.smart_cofee_hub
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:smart_cofee_hub/veiws/screens/screen_app_splash.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Smart Coffee Hub',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: ScreenAppSplash(),
+    );
+  }
+}
